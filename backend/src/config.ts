0e756937f3f7c350baec1f802069dcd5e1db6d51
Amine Achouri <achouri_amine@hotmail.fr>
2025-11-29 05:05:40 +0100

    fix: remove dotenv-safe configuration for improved simplicity

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,10 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const dotenvSafe = require('dotenv-safe');
-dotenvSafe.config({ allowEmptyValues: false });
-
 export const config = {
   nodeEnv: process.env.NODE_ENV ?? 'development',
   port: Number(process.env.PORT ?? 3000),
